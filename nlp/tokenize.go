6f3015a7681f6d4be025e06d3c756207b53cb464
amitl <amitl@amitl-lpt.roam.corp.google.com>
2016-02-21 12:24:04 +0200

    exported tokenizer regexp in nlp

--- nlp/tokenize.go
+++ nlp/tokenize.go
@@ -6,15 +6,16 @@ import (
 	"strings"
 )
 
-// Splits text into tokens.
-var tokenSplitter = regexp.MustCompile("\\w([\\w']*\\w)?")
+// Splits text into tokens. This regexp represents a single word.
+// Changing this regexp will affect the Tokenize function.
+var Tokenizer = regexp.MustCompile("\\w([\\w']*\\w)?")
 
 // Splits a given text to a slice of stemmed, lowercase words. If keepStopWords
 // is false, will drop stop words.
 func Tokenize(s string, keepStopWords bool) []string {
 	s = correctUtf8Punctuation(s)
 	s = strings.ToLower(s)
-	words := tokenSplitter.FindAllString(s, -1)
+	words := Tokenizer.FindAllString(s, -1)
 	var result []string
 	for _, word := range words {
 		if !keepStopWords && StopWords[word] {
