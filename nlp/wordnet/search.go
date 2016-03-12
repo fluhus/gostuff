862a827cf1a1f2958676f844d8c6c615580c3c25
Amit Lavon <doctor_troll@walla.co.il>
2016-03-12 19:13:21 +0200

    wordnet - fmt

--- nlp/wordnet/search.go
+++ nlp/wordnet/search.go
@@ -5,11 +5,12 @@ package wordnet
 func (wn *Wordnet) Search(word string) map[string][]*Synset {
 	result := map[string][]*Synset{}
 	for _, pos := range [...]string{"a", "n", "r", "v"} {
-		ids := wn.Lemma[pos + "." + word]
+		ids := wn.Lemma[pos+"."+word]
 		result[pos] = make([]*Synset, len(ids))
 		for i, id := range ids {
 			result[pos][i] = wn.Synset[id]
 		}
 	}
+	// TODO(amit): Search in exceptions too.
 	return result
 }
