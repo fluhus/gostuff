720dc4ed72348726ba1380903527319b236898ef
Amit Lavon <doctor_troll@walla.co.il>
2016-03-13 22:35:22 +0200

    wordnet - improved docs

--- nlp/wordnet/string.go
+++ nlp/wordnet/string.go
@@ -9,11 +9,14 @@ import (
 
 // TODO(amit): Consider removing String() functions to simplify the API.
 
+// Returns a compact string representation of the wordnet data collection, for
+// debugging.
 func (w *Wordnet) String() string {
 	return fmt.Sprintf("Wordnet[%d lemmas, %d synsets, %d exceptions]",
 		len(w.Lemma), len(w.Synset), len(w.Exception))
 }
 
+// Returns a string representation of the synset, for debugging.
 func (s *Synset) String() string {
 	result := bytes.NewBuffer(make([]byte, 0, 100))
 	fmt.Fprintf(result, "Synset[%s.", s.Pos)
