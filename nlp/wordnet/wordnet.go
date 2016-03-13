cb525cef35f6f0832240470612d812b88fb3dc73
Amit Lavon <doctor_troll@walla.co.il>
2016-03-13 22:31:11 +0200

    wordnet - merged indexer.go into wordnet.go

--- nlp/wordnet/wordnet.go
+++ nlp/wordnet/wordnet.go
@@ -31,6 +31,7 @@ package wordnet
 
 import (
 	"math"
+	"sort"
 )
 
 // Parses an entire wordnet directory. Path is the root of the directory.
@@ -182,4 +183,25 @@ func maxSynsetDistance(m map[*Synset]int) int {
 		}
 	}
 	return result
+}
+
+// Indexes all words in the data.
+func (wn *Wordnet) indexLemma() {
+	wn.Lemma = map[string][]string{}
+
+	// Sort synsets to keep index stable.
+	ids := make([]string, 0, len(wn.Synset))
+	for id := range wn.Synset {
+		ids = append(ids, id)
+	}
+	sort.Strings(ids)
+
+	for _, id := range ids {
+		ss := wn.Synset[id]
+		pos := id[0:1]
+		for _, word := range ss.Word {
+			w := pos + "." + word
+			wn.Lemma[w] = append(wn.Lemma[w], id)
+		}
+	}
 }
