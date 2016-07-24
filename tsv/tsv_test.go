a40ae5ac6d68a1077973c496454e4bd1f75dd199
amitl <amitl@amitl-lpt.roam.corp.google.com>
2016-07-24 08:31:09 +0300

    updated csv test

--- tsv/tsv_test.go
+++ tsv/tsv_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestDecoder_struct(t *testing.T) {
-	decoder := NewDecoder(strings.NewReader("Hello\t1\t-1\t3.14"), 0, 0)
+	decoder := NewDecoder(strings.NewReader("Hello,1,-1,3.14"), 0, 0)
 
 	act := struct {
 		S string
@@ -32,7 +32,7 @@ func TestDecoder_struct(t *testing.T) {
 }
 
 func TestDecoder_uintSlice(t *testing.T) {
-	decoder := NewDecoder(strings.NewReader("2\t3\t5\t7\t11\t13"), 0, 0)
+	decoder := NewDecoder(strings.NewReader("2,3,5,7,11,13"), 0, 0)
 	var act []uint
 	exp := []uint{2, 3, 5, 7, 11, 13}
 
@@ -53,7 +53,7 @@ func TestDecoder_uintSlice(t *testing.T) {
 }
 
 func TestDecoder_intSlice(t *testing.T) {
-	decoder := NewDecoder(strings.NewReader("2\t-3\t5\t-7\t11\t-13"), 0, 0)
+	decoder := NewDecoder(strings.NewReader("2,-3,5,-7,11,-13"), 0, 0)
 	var act []int
 	exp := []int{2, -3, 5, -7, 11, -13}
 
@@ -74,7 +74,7 @@ func TestDecoder_intSlice(t *testing.T) {
 }
 
 func TestDecoder_floatSlice(t *testing.T) {
-	decoder := NewDecoder(strings.NewReader("2\t-3.14\t5.5\t-7008\t0.11\t-1.3"),
+	decoder := NewDecoder(strings.NewReader("2,-3.14,5.5,-7008,0.11,-1.3"),
 		0, 0)
 	var act []float64
 	exp := []float64{2, -3.14, 5.5, -7008, 0.11, -1.3}
@@ -96,7 +96,7 @@ func TestDecoder_floatSlice(t *testing.T) {
 }
 
 func TestDecoder_stringSlice(t *testing.T) {
-	decoder := NewDecoder(strings.NewReader("yar\thar\tfiddle\tdi\tdee"),
+	decoder := NewDecoder(strings.NewReader("yar,har,fiddle,di,dee"),
 		0, 0)
 	var act []string
 	exp := []string{"yar", "har", "fiddle", "di", "dee"}
