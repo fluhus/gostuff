be82444370406e0fff8b74be5ae407572b5d81a9
Amit Lavon <amitlavon@cs.huji.ac.il>
2016-11-01 11:00:37 +0200

    wordnet - improved json compatibility

--- nlp/wordnet/types.go
+++ nlp/wordnet/types.go
@@ -3,46 +3,46 @@ package wordnet
 // An entire wordnet database.
 type WordNet struct {
 	// Maps from synset ID to synset.
-	Synset map[string]*Synset
+	Synset map[string]*Synset `json:"synset"`
 
 	// Maps from pos.lemma to synset IDs that contain it.
-	Lemma map[string][]string
+	Lemma map[string][]string `json:"lemma"`
 
 	// Like Lemma, but synsets are ordered from the most frequently used to the
 	// least. Only a subset of the synsets are ranked, so LemmaRanked has less
 	// synsets.
-	LemmaRanked map[string][]string
+	LemmaRanked map[string][]string `json:"lemmaRanked"`
 
 	// Maps from exceptional word to its forms.
-	Exception map[string][]string
+	Exception map[string][]string `json:"exception"`
 
 	// Maps from example ID to sentence template. Using string keys for JSON
 	// compatibility.
-	Example map[string]string
+	Example map[string]string `json:"example"`
 }
 
 // A set of synonymous words.
 type Synset struct {
 	// Synset offset, also used as an identifier.
-	Offset int
+	Offset int `json:"offset"`
 
 	// Part of speech, including 's' for adjective satellite.
-	Pos string
+	Pos string `json:"pos"`
 
 	// Words in this synset.
-	Word []string
+	Word []string `json:"word"`
 
 	// Pointers to other synsets.
-	Pointer []*Pointer
+	Pointer []*Pointer `json:"pointer"`
 
 	// Sentence frames for verbs.
-	Frame []*Frame
+	Frame []*Frame `json:"frame"`
 
 	// Lexical definition.
-	Gloss string
+	Gloss string `json:"gloss"`
 
 	// Usage examples for words in this synset. Verbs only.
-	Example []*Example
+	Example []*Example `json:"example"`
 }
 
 // Links a synset word to a generic phrase that illustrates how to use it.
@@ -52,10 +52,10 @@ type Synset struct {
 // https://wordnet.princeton.edu/man/wninput.5WN.html#sect4
 type Frame struct {
 	// Index of word in the containing synset, -1 for entire synset.
-	WordNumber int
+	WordNumber int `json:"wordNumber"`
 
 	// Frame number on the WordNet site.
-	FrameNumber int
+	FrameNumber int `json:"frameNumber"`
 }
 
 // Denotes a semantic relation between one synset/word to another.
@@ -65,23 +65,23 @@ type Frame struct {
 type Pointer struct {
 	// Relation between the 2 words. Target is <symbol> to source. See
 	// package constants for meaning of symbols.
-	Symbol string
+	Symbol string `json:"symbol"`
 
 	// Target synset ID.
-	Synset string
+	Synset string `json:"synset"`
 
 	// Index of word in source synset, -1 for entire synset.
-	Source int
+	Source int `json:"source"`
 
 	// Index of word in target synset, -1 for entire synset.
-	Target int
+	Target int `json:"target"`
 }
 
 // Links a synset word to an example sentence. Applies to verbs only.
 type Example struct {
 	// Index of word in the containing synset.
-	WordNumber int
+	WordNumber int `json:"wordNumber"`
 
 	// Number of template in the WordNet.Example field.
-	TemplateNumber int
+	TemplateNumber int `json:"templateNumber"`
 }
