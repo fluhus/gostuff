613d595d81bba91313b9e26b1c8117f145925a46
Amit Lavon <amit.lavon@weizmann.ac.il>
2023-11-24 16:44:58 +0200

    Add benchmark and fuzz for UPGMA

--- clustering/upgma_test.go
+++ clustering/upgma_test.go
@@ -1,9 +1,12 @@
 package clustering
 
 import (
+	"fmt"
 	"math"
 	"reflect"
 	"testing"
+
+	"github.com/fluhus/gostuff/gnum"
 )
 
 func TestUPGMA(t *testing.T) {
@@ -46,4 +49,35 @@ func TestUPGMA_more(t *testing.T) {
 			t.Errorf("Step(%v)=%v, want %v", i, step, steps[i])
 		}
 	}
+}
+
+func BenchmarkUPGMA(b *testing.B) {
+	for _, n := range []int{10, 30, 100} {
+		b.Run(fmt.Sprint(n), func(b *testing.B) {
+			nums := make([]float64, n)
+			for i := range nums {
+				nums[i] = 1.0 / float64(i+1)
+				if i%2 == 0 {
+					nums[i] += 10
+				}
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				upgma(n, func(i1, i2 int) float64 {
+					return gnum.Abs(nums[i1] - nums[i2])
+				})
+			}
+		})
+	}
+}
+
+func FuzzUPGMA(f *testing.F) {
+	f.Add(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+	f.Fuzz(func(t *testing.T, a float64, b float64, c float64,
+		d float64, e float64, f float64, g float64, h float64, i float64) {
+		nums := []float64{a, b, c, d, e, f, g, h, i}
+		upgma(len(nums), func(i1, i2 int) float64 {
+			return gnum.Abs(nums[i1] - nums[i2])
+		})
+	})
 }
