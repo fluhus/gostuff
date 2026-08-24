ae70638743f7fead0a3527eec44a9413941b80fa
fluhus <amitlavon1.spam@gmail.com>
2026-08-24 16:02:09 -0700

    Deprecate nlp package

--- nlp/doc.go
+++ nlp/doc.go
@@ -1,2 +1,7 @@
 // Package nlp provides basic NLP utilities.
+//
+// Deprecated: with the rise of newer NLP techniques (BM25, LLMs),
+// the tools in this package are no longer the way to go when analyzing text.
+// This package will be removed in the future, with the exception of the
+// wordnet package.
 package nlp
